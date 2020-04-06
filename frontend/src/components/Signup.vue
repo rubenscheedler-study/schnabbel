df964509aebcdcfe82a8c8e26e6d04cbc17f496a
ruben <r.j.scheedler@student.rug.nl>
2020-04-06 22:09:19 +0200

    add a form for signing up

---
+++ frontend/src/components/Signup.vue
@@ -0,0 +1,88 @@
+<template>
+  <div id="signup-form-container" class="container">
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <!-- e-mail -->
+      <b-form-group
+        id="input-group-1"
+        label="E-mailadres:"
+        label-for="input-1"
+        description=""
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          required
+          placeholder="jan.de.vries@gmail.com"
+        ></b-form-input>
+      </b-form-group>
+      <!-- password -->
+      <b-form-group
+        id="input-group-2"
+        label="Wachtwoord:"
+        label-for="input-2"
+        description="Maak er een lekker lange zin van. Dat is veilig!"
+      >
+        <b-form-input
+          id="input-2"
+          v-model="form.password"
+          type="password"
+          required
+        ></b-form-input>
+      </b-form-group>
+      <!-- username -->
+      <b-form-group id="input-group-3" label="Gebruikersnaam:" label-for="input-3">
+        <b-form-input
+          id="input-3"
+          v-model="form.username"
+          required
+          placeholder="Borrelnootje98"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Inschrijven</b-button>
+      <b-button type="reset" variant="danger">Formulier leegmaken</b-button>
+    </b-form>
+    <b-card class="mt-3" header="Form Data Result">
+      <pre class="m-0">{{ form }}</pre>
+    </b-card>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        form: {
+          email: '',
+          password: '',
+          username: ''
+        },
+        show: true
+      }
+    },
+    methods: {
+      onSubmit(evt) {
+        evt.preventDefault()
+        alert(JSON.stringify(this.form))
+      },
+      onReset(evt) {
+        evt.preventDefault()
+        // Reset our form values
+        this.form.email = ''
+        this.form.password = ''
+        this.form.username = ''
+        // Trick to reset/clear native browser form validation state
+        this.show = false
+        this.$nextTick(() => {
+          this.show = true
+        })
+      }
+    }
+  }
+</script>
+<style scoped>
+#signup-form-container {
+    padding-top: 50px;
+}
+</style>
--- frontend/src/routes/router.js
+++ frontend/src/routes/router.js
@@ -1,6 +1,7 @@
 import VueRouter from 'vue-router'
 import Home from '../components/Home'
 import Login from '../components/Login'
+import Signup from '../components/Signup'
 
 // https://stackoverflow.com/questions/45370112/vue-js-separate-routes-in-different-files
 /**
@@ -11,5 +12,6 @@ export default new VueRouter({routes:
     [
     { path: '/login', component: Login },
     { path: '/home', component: Home },
+    { path: '/signup', component: Signup}
     ]
 });
