c8063f7728aaef8b5e24280685fe46f3504d7dac
ruben <r.j.scheedler@student.rug.nl>
2020-04-06 20:50:15 +0200

    Add basic framework for the frontend.

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <Home/>
+  </div>
+</template>
+<script>
+ import Home from './components/Home.vue'
+  import Navbar from './components/Navbar.vue';  
+  export default {
+    name: 'navbar',
+    components: {
+      Navbar,
+      Home
+    }
+  }
+</script>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+      <b-navbar id="main-navbar" toggleable="lg" type="dark">
+        <b-container>
+            <b-navbar-brand href="#">Schnabbel</b-navbar-brand>
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+            <b-collapse id="nav-collapse" is-nav>
+              <!-- Right aligned nav items -->
+              <b-navbar-nav class="ml-auto">
+                <b-nav-form>
+                  <b-form-input 
+                    size="sm" 
+                    class="mr-sm-2" 
+                    placeholder="Search for a meal"
+                    v-model="meal"
+                    ></b-form-input>
+                  <b-button 
+                    size="sm" 
+                    class="my-2 my-sm-0" 
+                    type="submit" 
+                    @click.prevent="getMeal"
+                    >Search</b-button>
+                </b-nav-form>
+                <b-nav-item-dropdown right>
+                  <!-- Using 'button-content' slot -->
+                  <template slot="button-content"><em>User</em></template>
+                  <b-dropdown-item href="#">Profile</b-dropdown-item>
+                  <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+                </b-nav-item-dropdown>
+              </b-navbar-nav>
+            </b-collapse>
+          </b-container>
+      </b-navbar>
+    </div>  
+</template>
+<script>
+export default {
+    data() {
+        return {
+            meal: ''
+        }
+    },
+  methods: {
+    getMeal() {
+      return this.meal;
+    }
+  }
+}    
+</script>
+<style>
+#main-navbar {
+  background-color: #0b7070;
+}
+</style>
